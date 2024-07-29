6f1dbf8bfd110a5377fb568d2da79b37e9552e00
Manmoksh <mamoksh04@gmail.com>
2024-07-29 21:21:34 +0530

    making  a dashboard using html and css

---
+++ orders.js
@@ -0,0 +1,50 @@
+const orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Larvender KF102 Drone",
+    productNumber: "34631",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Ruko F11 Pro Drone",
+    productNumber: "49331",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Drone with Camera",
+    productNumber: "96631",
+    paymentStatus: "Paid",
+    shipping: "Pending",
+  },
+  {
+    productName: "GPS 4K Drone",
+    productNumber: "22821",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Djl Air xS",
+    productNumber: "81431",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable  Drone",
+    productNumber: "98031",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Lozenge Drone",
+    productNumber: "00431",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+];
